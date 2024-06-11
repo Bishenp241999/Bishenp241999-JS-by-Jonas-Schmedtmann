f2f85b5c35fc627dcb3c2ec724f997e45ef27a95
Bishenp241999 <praptibishen1999@gmail.com>
2024-06-11 12:45:54 +0530

    Third Commit Implementation of Pagination - part - 2

--- Forkify-app/src/js/controller.js
+++ Forkify-app/src/js/controller.js
@@ -7,6 +7,8 @@ import 'regenerator-runtime/runtime';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import { async } from 'regenerator-runtime';
+import paginationView from './views/paginationView.js';
+
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -36,22 +38,32 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
-   resultsView.renderSpinner();
+    resultsView.renderSpinner();
     // 1) Get search query
     const query = searchView.getQuery();
     // console.log(query);
     if (!query) return;
-    
+
 
     // 2) Load search results
     await model.loadSearchResults(query);
 
     // 3) Render results
     // console.log(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage(1))
+    resultsView.render(model.getSearchResultsPage(1));
+
+    // 4 render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
     console.log(error);
   }
+}
+
+const controlPagination = function (goToPage) {
+  // console.log(goToPage);
+  // render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search)
 }
 
 // alternative for line 146 & 147
@@ -61,5 +73,6 @@ const controlSearchResults = async function () {
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 }
 init();
